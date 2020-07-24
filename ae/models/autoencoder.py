3fd3bcbf4e51f62b286d7e6308bce139f208bdf0
al1729 <andyliu1708@gmail.com>
2020-07-24 18:03:35 -0500

    week update

--- ae/models/autoencoder.py
+++ ae/models/autoencoder.py
@@ -11,7 +11,8 @@ class AutoEncoder:
         self.encoding_dim = encoding_dim
         self.input_shape = input_shape
 
-    def build_model(self, encoded1_shape, encoded2_shape, decoded1_shape, decoded2_shape):
+    #def build_model(self, encoded1_shape, encoded2_shape, decoded1_shape, decoded2_shape):
+    def build_model(self):
         input_data = Input(shape=(1, self.input_shape))
 
         # encoded1 = Dense(encoded1_shape, activation="relu", activity_regularizer=regularizers.l2(0))(input_data)
@@ -22,7 +23,7 @@ class AutoEncoder:
         # decoded = Dense(self.input_shape, activation="sigmoid", activity_regularizer=regularizers.l2(0))(decoded2)
 
         encoded3 = Dense(self.encoding_dim, activation="relu", activity_regularizer=regularizers.l2(0))(input_data)
-        decoded = Dense(self.input_shape, activation="sigmoid", activity_regularizer=regularizers.l2(0))(encoded3)
+        decoded = Dense(self.input_shape, activation="relu", activity_regularizer=regularizers.l2(0))(encoded3)
 
         self.autoencoder = Model(inputs=input_data, outputs=decoded)
         self.encoder = Model(input_data, encoded3)
@@ -47,16 +48,17 @@ class AutoEncoder:
 
         print(model.evaluate(test_data, test_data))
         
+       
     def encode_data(self, data, csv_path, save_csv=True):
         coded_train = []
         for i in range(len(data)):
             curr_data = np.array(data.iloc[i, :])
             values = np.reshape(curr_data, (1, 1, self.input_shape))
             coded = self.encoder.predict(values)
-            shaped = np.reshape(coded, (20,))
+            shaped = np.reshape(coded, (self.encoding_dim,))
             coded_train.append(shaped)
 
-        train_coded = pd.DataFrame(coded_train, index=np.arange(len(coded_train)), columns=np.arange(20))
+        train_coded = pd.DataFrame(coded_train, index=np.arange(len(coded_train)), columns=np.arange(self.encoding_dim))
         if save_csv:
             train_coded.to_csv(f"{csv_path}")
         return train_coded
