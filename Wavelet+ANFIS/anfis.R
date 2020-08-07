164668cfee82f573525c1d12e496969382726fa7
al1729 <andyliu1708@gmail.com>
2020-08-07 18:29:30 -0500

    Revert "wavelet+anfis progress 8/7"
    This reverts commit 17b71a19ec7ac5dcc1bc0048e0432eac366073f6.

--- Wavelet+ANFIS/anfis.R
+++
@@ -1,8 +0,0 @@
-#read data
-setwd("/Users/andyliu/develop/andy/Wavelet+ANFIS")
-train_x <- read.csv(file = 'train_x.csv')
-test_x <- read.csv(file = 'test_x.csv')
-train_y <- read.csv(file = 'train_y.csv')
-test_y <- read.csv(file = 'test_y.csv')
-train <- cbind(train_x, test_x)
-
